98e83c780c8f0a1755e6f10920db9334816c5d5b
sergzv-dev <serg.zv.dev@gmail.com>
2026-01-06 19:02:37 +0400

    add pool

--- worker_db_conn.py
+++ worker_db_conn.py
@@ -3,26 +3,27 @@ from worker import Worker
 import json
 import os
 from dotenv import load_dotenv
-from multiprocessing import Process
+from multiprocessing import Pool
 
 load_dotenv()
 HOST = os.getenv('REDIS_HOST')
 PORT = int(os.getenv('REDIS_PORT'))
 
+def worker(task):
+    task_id = task['task_id']
+    data = task['data']
 
-def worker():
-    redis_client = redis.Redis(host=HOST, port=PORT, decode_responses=True)
+    w_redis_client = redis.Redis(host=HOST, port=PORT, decode_responses=True)
+    res = Worker.execute(data)
+    w_redis_client.hset('done_tasks', task_id, res)
 
-    while True:
-        _, payload = redis_client.brpop('active_tasks')
-        task = json.loads(payload)
-        task_id = task['task_id']
-        data = task['data']
-
-        res = Worker.execute(data)
-        redis_client.hset('done_tasks', task_id, res)
 
 if __name__ == '__main__':
-    for _ in range(4):
-        Process(target=worker).start()
+    redis_client = redis.Redis(host=HOST, port=PORT, decode_responses=True)
 
+    with Pool(processes=4) as pool:
+
+        while True:
+            _, payload = redis_client.brpop('active_tasks')
+            task = json.loads(payload)
+            pool.apply_async(worker, args=(task,))
