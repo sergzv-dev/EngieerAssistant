c1f97df34d6ac5bf743a1b3d13a39d381b547eb2
sergzv-dev <serg.zv.dev@gmail.com>
2026-01-06 16:25:42 +0400

    add processes

--- worker_db_conn.py
+++ worker_db_conn.py
@@ -3,18 +3,26 @@ from worker import Worker
 import json
 import os
 from dotenv import load_dotenv
+from multiprocessing import Process
 
 load_dotenv()
 HOST = os.getenv('REDIS_HOST')
 PORT = int(os.getenv('REDIS_PORT'))
 
-redis_client = redis.Redis(host=HOST, port=PORT, decode_responses=True)
 
-while True:
-    _, payload = redis_client.brpop('active_tasks')
-    task = json.loads(payload)
-    task_id = task['task_id']
-    data = task['data']
+def worker():
+    redis_client = redis.Redis(host=HOST, port=PORT, decode_responses=True)
 
-    res = Worker.execute(data)
-    redis_client.hset('done_tasks', task_id, res)
+    while True:
+        _, payload = redis_client.brpop('active_tasks')
+        task = json.loads(payload)
+        task_id = task['task_id']
+        data = task['data']
+
+        res = Worker.execute(data)
+        redis_client.hset('done_tasks', task_id, res)
+
+if __name__ == '__main__':
+    for _ in range(4):
+        Process(target=worker).start()
+
